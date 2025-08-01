c4587fad2417ddab3087da37130e9128914118ec
steve <rperego@proton.me>
2025-08-01 21:39:50 +0200

    updating_casks

--- Casks/ao-busterse.rb
+++ Casks/ao-busterse.rb
@@ -2,7 +2,8 @@ cask "ao-busterse" do
   version "7.0"
   sha256 "f91fe76dd9d20a47bb40399cee74db3763ae0bf55577a2afcc6db692a5c5a98e"
 
-  url "https://analogobsession.com/wp-content/uploads/2022/12/BUSTERse_#{version}.pkg"
+  url "https://analogobsession.com/wp-content/uploads/2022/12/BUSTERse_#{version}.pkg",
+    verified: "analogobsession.com/"
   name "BUSTERse"
   desc "Classic console compressor with extra sidechain options"
   homepage "https://www.patreon.com/posts/busterse-42658623"
